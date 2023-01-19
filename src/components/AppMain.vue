f748a303919856e7852f0aead80bab4f60e0a9f4
GtDavide01 <davidefiorini01@gmail.com>
2023-01-19 15:33:03 +0100

    Bug : non  mi tornano i progetti

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,40 @@
 <script>
+import axios from "axios";
 export default {
   name: "AppMain",
+
+  data() {
+    return {
+      // variabile che conterra tutti i progetti presi da axios
+      projects: [],
+      //url per fare chiamata axios
+      urlAxios: "http://localhost:8000",
+    };
+  },
+  created() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get(`${this.urlAxios}/api/projects`).then((resp) => {
+        this.projects = resp.data.results;
+      });
+      console.log(this.projects);
+    },
+  },
 };
 </script>
 
 <template>
   <main>
-    <H2>CIAO</H2>
+    <div class="container">
+      <h2>Tutti i progetti!</h2>
+    </div>
   </main>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+main {
+  background-color: #add8ff;
+}
+</style>
