59b615e7463fb9e33776eb484ae32be9cd343177
GtDavide01 <davidefiorini01@gmail.com>
2023-01-19 15:05:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  name: "App",
+  components: { AppHeader },
+};
+</script>
+
+<template>
+  <!-- header -->
+  <AppHeader />
+  <!-- main -->
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header>
+    <h1>Vite-Boolfolio</h1>
+  </header>
+</template>
+
+<style lang="scss">
+header {
+  height: 80px;
+  background-color: #0078d4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    color: white;
+  }
+}
+</style>
